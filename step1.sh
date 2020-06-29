34eb438ae98c0d0a57323aacebd315c521885924
Matze <66997348+MatzeNbg@users.noreply.github.com>
2020-06-29 15:39:07 +0200

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -3,4 +3,6 @@ sudo apt-get -y install nginx php7.0-fpm php7.0-cgi php7.0-xml php7.0-sqlite3 ph
 sudo systemctl disable lighttpd
 sudo systemctl enable php7.0-fpm
 sudo systemctl enable nginx
-nano /etc/nginx/sites-available/default
+curl https://raw.githubusercontent.com/MatzeNbg/pihole-change2nginx-1/master/nginx-default-pihole
+sudo rm /etc/nginx/sites-available/default
+sudo mv nginx-default-pihole /etc/nginx/sites-available/default
